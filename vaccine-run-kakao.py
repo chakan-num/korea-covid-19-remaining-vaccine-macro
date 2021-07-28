b2b199bfd47c57ecdde57edb4fa62ef8d8184e9a
simon <goodism5082@gmail.com>
2021-07-28 12:38:06 +0900

    roll back origin code

--- vaccine-run-kakao.py
+++ vaccine-run-kakao.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import asyncio
-import webbrowser
-
 import browser_cookie3
 import requests
 import configparser
@@ -14,11 +11,8 @@ from playsound import playsound
 from datetime import datetime
 
 import urllib3
-from requests import Session
-from requests.adapters import HTTPAdapter
-from urllib3 import Retry
 
-search_time = 0.1  # 잔여백신을 해당 시간마다 한번씩 검색합니다. 단위: 초
+search_time = 0.2  # 잔여백신을 해당 시간마다 한번씩 검색합니다. 단위: 초
 urllib3.disable_warnings()
 
 jar = browser_cookie3.chrome(domain_name=".kakao.com")
@@ -54,52 +48,32 @@ def load_config():
             else:
                 return None, None, None, None, None
         except ValueError:
-            print("config 에러 발생 : " + ValueError)
             return None, None, None, None, None
     return None, None, None, None, None
 
 
 def check_user_info_loaded():
-    print("사용자 정보를 불러오고 있습니다.")
-
-    while True:
-        global jar
-        user_info_api = 'https://vaccine.kakao.com/api/v1/user'
-        user_info_response = requests.get(user_info_api, headers=Headers.headers_vacc, cookies=jar, verify=False)
-        jar = user_info_response.cookies
-        user_info_json = json.loads(user_info_response.text)
-        if user_info_json.get('error'):
-            print("사용자 정보를 불러오는데 실패하였습니다.")
-            print("Chrome 브라우저에서 카카오에 제대로 로그인되어있는지 확인해주세요.")
-            print("로그인이 되어 있는데도 안된다면, 카카오톡에 들어가서 잔여백신 알림 신청을 한번 해보세요. 정보제공 동의가 나온다면 동의 후 다시 시도해주세요.")
-
-            webbrowser.open('https://accounts.kakao.com/login?continue=https://vaccine-map.kakao.com/map2?v=1')
-            login_try = str.lower(input("로그인을 완료하셨습니까? Y/N"))
-            if login_try == "y":
+    user_info_api = 'https://vaccine.kakao.com/api/v1/user'
+    user_info_response = requests.get(user_info_api, headers=Headers.headers_vacc, cookies=jar, verify=False)
+    user_info_json = json.loads(user_info_response.text)
+    if user_info_json.get('error'):
+        print("사용자 정보를 불러오는데 실패하였습니다.")
+        print("Chrome 브라우저에서 카카오에 제대로 로그인되어있는지 확인해주세요.")
+        print("로그인이 되어 있는데도 안된다면, 카카오톡에 들어가서 잔여백신 알림 신청을 한번 해보세요. 정보제공 동의가 나온다면 동의 후 다시 시도해주세요.")
+        close()
+    else:
+        user_info = user_info_json.get("user")
+        for key in user_info:
+            value = user_info[key]
+            # print(key, value)
+            if key != 'status':
                 continue
-            else:
-                close()
-        else:
-            user_info = user_info_json.get("user")
-
-            if user_info['status'] == "NORMAL":
+            if key == 'status' and value == "NORMAL":
                 print("사용자 정보를 불러오는데 성공했습니다.")
                 break
             else:
                 print("이미 접종이 완료되었거나 예약이 완료된 사용자입니다.")
                 close()
-
-'''            for key in user_info:
-                value = user_info[key]
-                if key != 'status':
-                    continue
-                if key == 'status' and value == "NORMAL":
-                    print("사용자 정보를 불러오는데 성공했습니다.")
-                    break
-                else:
-                    print("이미 접종이 완료되었거나 예약이 완료된 사용자입니다.")
-                    close()
-'''
 
 
 def input_config():
@@ -111,30 +85,27 @@ def input_config():
         print("아스크라제네카   : VEN00015")
         print("얀센          : VEN00016")
         print("아무거나       : ANY")
-        #vaccine_type = str.upper(input("예약시도할 백신 코드를 알려주세요."))
-        vaccine_type = "VEN00013"
+        vaccine_type = str.upper(input("예약시도할 백신 코드를 알려주세요."))
 
     print("사각형 모양으로 백신범위를 지정한 뒤, 해당 범위 안에 있는 백신을 조회해서 남은 백신이 있으면 Chrome 브라우저를 엽니다.")
     top_x = None
     while top_x is None:
-        #top_x = input(f"사각형의 위쪽 좌측 x값을 넣어주세요. 127.xxxxxx: ").strip()
-        top_x = 126.90847817816424
+        top_x = input(f"사각형의 위쪽 좌측 x값을 넣어주세요. 127.xxxxxx: ").strip()
 
     top_y = None
     while top_y is None:
-        #top_y = input(f"사각형의 위쪽 좌측 y값을 넣어주세요 37.xxxxxx: ").strip()
-        top_y = 37.510030658018486
+        top_y = input(f"사각형의 위쪽 좌측 y값을 넣어주세요 37.xxxxxx: ").strip()
+        top_y = top_y
 
     bottom_x = None
     while bottom_x is None:
-        #bottom_x = input(f"사각형의 아래쪽 우측 x값을 넣어주세요 127.xxxxxx: ").strip()
-        bottom_x = 126.86468150658285
+        bottom_x = input(f"사각형의 아래쪽 우측 x값을 넣어주세요 127.xxxxxx: ").strip()
 
     bottom_y = None
     while bottom_y is None:
-        #bottom_y = input(f"사각형의 아래쪽 우측 y값을 넣어주세요 37.xxxxxx: ").strip()
-        bottom_y = 37.54499553409828
+        bottom_y = input(f"사각형의 아래쪽 우측 y값을 넣어주세요 37.xxxxxx: ").strip()
 
+    dump_config(vaccine_type, top_x, top_y, bottom_x, bottom_y)
     return vaccine_type, top_x, top_y, bottom_x, bottom_y
 
 
@@ -153,7 +124,6 @@ def dump_config(vaccine_type, top_x, top_y, bottom_x, bottom_y):
 
 
 def close():
-    print("프로그램을 종료하겠습니다.")
     input("Press Enter to close...")
     sys.exit()
 
@@ -209,51 +179,33 @@ class Headers:
     }
 
 
-#async def try_reservation(organization_code, vaccine_type, aiohttp=None):
-async def try_reservation(vaccine_type, organization, aiohttp=None):
-
-    if organization.get('status') != "AVAILABLE" and organization.get('leftCounts') == 0:
-        return False
-
-    organization_code = organization.get('orgCode')
+def try_reservation(organization_code, vaccine_type):
     reservation_url = 'https://vaccine.kakao.com/api/v1/reservation'
-    data = {"from": "Map", "vaccineCode": vaccine_type, "orgCode": organization_code, "distance": "null"}
-
-    async with aiohttp.ClientSession() as session:
-        async with session.post(reservation_url, data=json.dumps(data), headers=Headers.headers_vacc, cookies=jar, verify=False) as response:
-            response_json = json.loads(await response.text())
-
-            '''
-            session = Session()
-            retry = Retry(total=3, backoff_factor=0, status_forecelist=[408,502,503,504])
-            session.mount('https://', HTTPAdapter(max_retries=retry))
-
-            reservation_url = 'https://vaccine.kakao.com/api/v1/reservation'
-            data = {"from": "Map", "vaccineCode": vaccine_type, "orgCode": organization_code, "distance": "null"}
-            response = session.post(reservation_url, data=json.dumps(data), headers=Headers.headers_vacc, cookies=jar, verify=False)
-            response_json = json.loads(response.text)
-            '''
-            print(response_json)
-
-            for key in response_json:
-                value = response_json[key]
-                if key != 'code':
-                    continue
-                if key == 'code' and value == "NO_VACANCY":
-                    print("잔여백신 접종 신청이 선착순 마감되었습니다.")
-                    return False
-        #            time.sleep(0.08)
-                elif key == 'code' and value == "SUCCESS":
-                    print("백신접종신청 성공!!!")
-                    organization_code_success = response_json.get("organization")
-                    print(
-                        f"병원이름: {organization_code_success.get('orgName')}\t전화번호: {organization_code_success.get('phoneNumber')}\t주소: {organization_code_success.get('address')}\t운영시간: {organization_code_success.get('openHour')}")
-                    play_tada()
-                    close()
-                else:
-                    print("ERROR. 아래 메시지를 보고, 예약이 신청된 병원 또는 1339에 예약이 되었는지 확인해보세요.")
-                    print(response.text)
-                    close()
+    for i in range(2):
+        data = {"from": "Map", "vaccineCode": vaccine_type, "orgCode": organization_code, "distance": "null"}
+        response = requests.post(reservation_url, data=json.dumps(data), headers=Headers.headers_vacc, cookies=jar,
+                                 verify=False)
+        response_json = json.loads(response.text)
+        print(response_json)
+        for key in response_json:
+            value = response_json[key]
+            if key != 'code':
+                continue
+            if key == 'code' and value == "NO_VACANCY":
+                print("잔여백신 접종 신청이 선착순 마감되었습니다.")
+                time.sleep(0.08)
+            elif key == 'code' and value == "SUCCESS":
+                print("백신접종신청 성공!!!")
+                organization_code_success = response_json.get("organization")
+                print(
+                    f"병원이름: {organization_code_success.get('orgName')}\t전화번호: {organization_code_success.get('phoneNumber')}\t주소: {organization_code_success.get('address')}\t운영시간: {organization_code_success.get('openHour')}")
+                play_tada()
+                close()
+            else:
+                print("ERROR. 아래 메시지를 보고, 예약이 신청된 병원 또는 1339에 예약이 되었는지 확인해보세요.")
+                print(response.text)
+                close()
+    return None
 
 
 # ===================================== def ===================================== #
@@ -269,8 +221,7 @@ async def try_reservation(vaccine_type, organization, aiohttp=None):
 #     print(cookie)
 
 
-
-async def find_vaccine(vaccine_type, top_x, top_y, bottom_x, bottom_y):
+def find_vaccine(vaccine_type, top_x, top_y, bottom_x, bottom_y):
     url = 'https://vaccine-map.kakao.com/api/v2/vaccine/left_count_by_coords'
     data = {"bottomRight": {"x": bottom_x, "y": bottom_y}, "onlyLeft": False, "order": "latitude",
             "topLeft": {"x": top_x, "y": top_y}}
@@ -280,21 +231,17 @@ async def find_vaccine(vaccine_type, top_x, top_y, bottom_x, bottom_y):
     while not done:
         try:
             time.sleep(search_time)
-
             response = requests.post(url, data=json.dumps(data), headers=Headers.headers_map, verify=False)
-            json_data = json.loads(response.text).get("organizations")
 
+            pretty_print(response.text)
             print(datetime.now())
-            print("조회 병원 수 : ", len(json_data))
 
-#            await asyncio.gather(try_reservation(x.get('orgCode'), vaccine_type) for x in json_data)
-            await asyncio.gather(*[try_reservation(vaccine_type, x) for x in json_data])
-
-            '''
+            json_data = json.loads(response.text).get("organizations")
             for x in json_data:
                 if x.get('status') == "AVAILABLE" or x.get('leftCounts') != 0:
-                    done = try_reservation(x.get('orgCode'), vaccine_type)
-            '''
+                    found = x
+                    done = True
+                    break
 
         except requests.exceptions.Timeout as timeouterror:
             print("Timeout Error : ", timeouterror)
@@ -316,6 +263,37 @@ async def find_vaccine(vaccine_type, top_x, top_y, bottom_x, bottom_y):
             print("AnyException : ", error)
             close()
 
+    if found is None:
+        find_vaccine(vaccine_type, top_x, top_y, bottom_x, bottom_y)
+    print(f"{found.get('orgName')} 에서 백신을 {found.get('leftCounts')}개 발견했습니다.")
+    print(f"주소는 : {found.get('address')} 입니다.")
+    organization_code = found.get('orgCode')
+
+    # 실제 백신 남은수량 확인
+    vaccine_found_code = None
+
+    if vaccine_type == "ANY":  # ANY 백신 선택
+        check_organization_url = f'https://vaccine.kakao.com/api/v2/org/org_code/{organization_code}'
+        check_organization_response = requests.get(check_organization_url, headers=Headers.headers_vacc, cookies=jar,
+                                                   verify=False)
+        check_organization_data = json.loads(check_organization_response.text).get("lefts")
+        for x in check_organization_data:
+            if x.get('leftCount') != 0:
+                found = x
+                print(f"{x.get('vaccineName')} 백신을 {x.get('leftCount')}개 발견했습니다.")
+                vaccine_found_code = x.get('vaccineCode')
+                break
+            else:
+                print(f"{x.get('vaccineName')} 백신이 없습니다.")
+
+    else:
+        vaccine_found_code = vaccine_type
+        print(f"{vaccine_found_code} 으로 예약을 시도합니다.")
+
+    if vaccine_found_code and try_reservation(organization_code, vaccine_found_code):
+        return None
+    else:
+        find_vaccine(vaccine_type, top_x, top_y, bottom_x, bottom_y)
 
 
 def main_function():
@@ -325,7 +303,7 @@ def main_function():
         vaccine_type, top_x, top_y, bottom_x, bottom_y = input_config()
     else:
         vaccine_type, top_x, top_y, bottom_x, bottom_y = previous_used_type, previous_top_x, previous_top_y, previous_bottom_x, previous_bottom_y
-    asyncio.run(find_vaccine(vaccine_type, top_x, top_y, bottom_x, bottom_y))
+    find_vaccine(vaccine_type, top_x, top_y, bottom_x, bottom_y)
     close()
 
 
